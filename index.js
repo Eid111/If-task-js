44ce8c6a120e1e53b1d46b7011981f44ca77cf86
Eid111 <eidnagy34@gmail.com>
2024-08-20 14:37:19 +0300

    If task js

---
+++ index.js
@@ -0,0 +1,138 @@
+/*
+//Qustion 1
+let num1=+prompt("Enter num1");
+
+
+if(num1<=100){
+    console.log("number is less than or equal to 100")
+}
+else
+{
+    console.log("number is greater than 100")
+} */
+//------------------------------------------------
+/* 
+//Qustion 2
+let text=prompt("Enter your text");
+let length=text.length;
+
+if(length>100){
+    console.log("the string is long")
+}
+else
+{
+    console.log("string is short")
+} 
+ */
+
+
+//-----------------------------------
+
+/* 
+//Qustion 3
+let password="rrt14255";
+let pas=prompt("Enter your password :-");
+if(pas===password){
+    console.log("Password is validat")
+}
+else{
+    console.log("no valied")
+} */
+
+
+//---------------------------------
+/* 
+//Qustion 4
+
+let day=prompt("Enter day name")
+var validat="saterday"
+if (day==validat){
+    console.log("It's weekend")
+}
+else{
+    console.log("work day")
+} */
+
+//--------------------------------
+/* 
+//Qustion 5
+
+
+let num=+prompt("Enter your nummber not equal 0")
+if (num>0){
+    console.log("Nummber is positive")
+}
+else {
+    console.log("Nummber is negative")
+} */
+
+
+
+//--------------------------------
+/* 
+//Qustion 6
+
+
+let grd=+prompt("Enter your grade between (0 , 100) !")   
+if (grd>=90){
+    console.log("The grad is A")
+}
+else if (grd>=80){
+    console.log("The grad is B")
+}
+else if (grd>=70){
+    console.log("The grad is C")
+}
+else if (grd>=60){
+    console.log("The grad is D")
+}
+else  {
+    console.log("The grad is F")
+} */
+
+
+//--------------------------------
+
+/* 
+//Qustion 7
+
+let temp=+prompt("Enter the tempreture")
+if (temp>=30){
+    console.log("It's Hot !!!!!")
+}
+else{
+    console.log("It's cold !!!!!")
+} */
+
+
+
+//--------------------------------
+/* 
+//Qustion 8
+
+let num=+prompt("Enter your nummber not equal 0")
+if (num%2==1){
+    console.log("Nummber is odd")
+}
+else{
+    console.log("Nummber is even")
+} */
+
+
+
+
+//--------------------------------
+//Qustion 9
+let age=+prompt("Enter your age must be grater than 20")
+
+if (age > 20) {
+    console.log("You can enter the university");
+    
+    if (age % 2 === 0) {
+        console.log(age + " is an even number.");
+    } else {
+        console.log(age + " is an odd number.");
+    }
+} else {
+    console.log("Age is not greater than 20.");
+}
